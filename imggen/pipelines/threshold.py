7883286781a64e1cd200681a32c95c64f2e61c41
Bruce Cowan <bruce@bcowan.me.uk>
2020-06-14 15:02:08 +0100

    Renumber regions

--- imggen/pipelines/threshold.py
+++ imggen/pipelines/threshold.py
@@ -7,19 +7,22 @@ import definitions
 from ..routines import plottools
 
 
-def _do_plotting(img: cueimgproc.Image, regions: np.ma.MaskedArray) -> figure.Figure:
+def _do_plotting(
+    img: cueimgproc.Image, binary: cueimgproc.Image, regions: np.ndarray
+) -> figure.Figure:
     fig, ax = plottools.create_subplots(0.6, 3, sharex="all")
 
     img.plot(ax[0])
     ax[0].set_title("Original image")
 
-    ax[1].imshow(np.invert(regions.mask), cmap="binary")
-    ax[1].set_title("Thresholded image")
+    binary.plot(ax[1])
+    ax[1].set_title("Binary image")
 
     cmap = colors.ListedColormap(sns.husl_palette(len(np.unique(regions))).as_hex())
     bounds = np.asarray(np.unique(regions.data))
     norm = colors.BoundaryNorm(bounds, cmap.N)
 
+    regions = np.ma.masked_equal(regions, 0, False)
     ax[2].imshow(regions, cmap=cmap, norm=norm)
     ax[2].set_title(f"Regions: {cmap.N}")
 
@@ -30,7 +33,7 @@ def plot_threshold() -> figure.Figure:
     img = cueimgproc.Image.open(definitions.DATA_DIR / "stepped.tiff")
     binary = img.filter(cueimgproc.ThresholdFilter(cueimgproc.ThresholdType.SAUVOLA))
     labelled_image = cueimgproc.LabelledImage(binary.data, img)
-    return _do_plotting(img, labelled_image.region_map)
+    return _do_plotting(img, binary, labelled_image.region_map)
 
 
 def plot_extra_threshold() -> figure.Figure:
@@ -45,4 +48,4 @@ def plot_extra_threshold() -> figure.Figure:
     idxs = eccentricities[eccentricities["eccentricity"] < 0.9].index.values
     region_img = labelled_image.filter_regions(idxs)
 
-    return _do_plotting(img, region_img)
+    return _do_plotting(img, binary, region_img)
