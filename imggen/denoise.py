e71a56f4a74b57cd31e880fc005701c9e4a2fb61
Bruce Cowan <bruce@bcowan.me.uk>
2020-06-17 13:40:34 +0100

    Adapt to denoise changes

--- imggen/denoise.py
+++ imggen/denoise.py
@@ -1,42 +1,38 @@
 import time
-from typing import Any, Callable
+from typing import Any
 
+import cueimgproc
 from matplotlib import axes, colors, figure
-import numpy as np
-from skimage import io, filters, restoration, util
 
 from .routines import plottools
 
 import definitions
 
 
-def _load_image() -> np.ndarray:
-    img = io.imread(definitions.DATA_DIR / "noisy.png")
-    return util.img_as_float(img)
-
-
 def _plot_denoise(
-    ax: axes.Axes, func: Callable, title="Denoised", **kwargs: Any
+    ax: axes.Axes, denoise_type: cueimgproc.DenoiseType, title: str, **kwargs: Any
 ) -> None:
-    img = _load_image()
+    img = cueimgproc.Image.open(definitions.DATA_DIR / "noisy.png")
+
+    denoiser = cueimgproc.DenoiseFilter(denoise_type, **kwargs)
 
     start = time.process_time()
-    denoised = func(img, **kwargs)
+    denoised = img.filter(denoiser)
     end = time.process_time()
 
-    ax.imshow(denoised, norm=colors.Normalize(0.0, 1.0))
+    denoised.plot(ax, norm=colors.Normalize(0.0, 1.0))
     ax.set(title=f"{title}\nTime: {(end - start) * 1000.0:.0f} ms")
 
 
 def _plot_original(ax: axes.Axes) -> None:
-    data = _load_image()
+    data = cueimgproc.Image.open(definitions.DATA_DIR / "noisy.png")
 
-    ax.imshow(data, norm=colors.Normalize(0.0, 1.0))
+    data.plot(ax, norm=colors.Normalize(0.0, 1.0))
     ax.set(title="Original image")
 
 
 def plot_denoise_all() -> figure.Figure:
-    fig, axs = plottools.create_subplots(1.2, 3, 2, sharex="all")
+    fig, axs = plottools.create_subplots(1, 3, 3, sharex="all", sharey="all")
     gs = axs[0][0].get_gridspec()
     for ax in axs[0, :]:
         ax.remove()
@@ -46,24 +42,12 @@ def plot_denoise_all() -> figure.Figure:
     plottools.remove_ticks(axs)
 
     _plot_original(ax_original)
-    _plot_denoise(axs[1, 0], filters.median, "Median filter")
-    _plot_denoise(
-        axs[1, 1], filters.gaussian, "Gaussian filter", multichannel=True, sigma=2.0
-    )
-    _plot_denoise(
-        axs[2, 0],
-        restoration.denoise_nl_means,
-        "Non-local means, $h=0.02$",
-        multichannel=True,
-        h=0.03,
-    )
-    _plot_denoise(
-        axs[2, 1],
-        restoration.denoise_wavelet,
-        "Wavelet",
-        multichannel=True,
-        convert2ycbcr=True,
-        rescale_sigma=True,
-    )
+
+    _plot_denoise(axs[1, 0], cueimgproc.DenoiseType.BILATERAL, "Bilateral")
+    _plot_denoise(axs[1, 1], cueimgproc.DenoiseType.MEDIAN, "Median")
+    _plot_denoise(axs[1, 2], cueimgproc.DenoiseType.NL_MEANS, "Non-local means", h=0.05)
+    _plot_denoise(axs[2, 0], cueimgproc.DenoiseType.TV_BREGMAN, "TV: Bregman", weight=1)
+    _plot_denoise(axs[2, 1], cueimgproc.DenoiseType.TV_CHAMBOLLE, "TV: Chambolle")
+    _plot_denoise(axs[2, 2], cueimgproc.DenoiseType.WAVELET, "Wavelet")
 
     return fig
