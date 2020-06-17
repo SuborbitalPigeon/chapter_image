93445c8ae6260fcd2b7054fc96535b53dd441053
Bruce Cowan <bruce@bcowan.me.uk>
2020-06-17 14:01:57 +0100

    Add files which were missing

---
+++ datagen/threshold.py
@@ -0,0 +1,26 @@
+import logging
+
+import cueimgproc
+
+import definitions
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    img = cueimgproc.Image.open(definitions.DATA_DIR / "stepped.tiff")
+    binary = img.filter(
+        cueimgproc.ThresholdFilter(cueimgproc.ThresholdType.SAUVOLA),
+        cueimgproc.RemoveSmallObjectsFilter(32),
+    )
+
+    labelled_image = cueimgproc.LabelledImage(binary.data, img)
+
+    regions = labelled_image.region_properties("centroid", "eccentricity")
+    regions = regions[regions["eccentricity"] < 0.9]
+
+    regions.to_csv(definitions.PROJECT_ROOT / "datagen" / "raw_data" / "threshold.csv")
+
+
+if __name__ == "__main__":
+    main()
